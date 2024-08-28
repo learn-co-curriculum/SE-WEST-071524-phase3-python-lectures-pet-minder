3fd3b899dd40492b57258196513f5f97981a44e4
betalantz <betalantz@gmail.com>
2024-08-28 13:04:23 -0700

    completes code 03

--- 03_oop/debug.py
+++ 03_oop/debug.py
@@ -2,5 +2,24 @@
 
 from lib.pet import *
 
+# brigid = Pet()
+# simon = Pet()
 
-import ipdb; ipdb.set_trace()
+# brigid.name = "Brigid"
+
+cookie = Pet("Cookie", 5, "Dachshund", "fiesty", "./assets/cookie.jpg")
+rose = Pet("Rose", 10, "domestic_longhair", "sweet", "./assets/rose.jpg")
+
+print(cookie)
+cookie.print_pet()
+
+rose.age = "ain't nothin' but a number"
+print(rose.age)
+rose.age = 11
+rose.age = 8
+print(rose.age)
+
+
+import ipdb
+
+ipdb.set_trace()
