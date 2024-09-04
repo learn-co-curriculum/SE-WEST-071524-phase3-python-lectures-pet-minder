7937f38b616c8fa0e2465e3c74c49d8469fd4f05
betalantz <betalantz@gmail.com>
2024-09-04 10:15:30 -0700

    resets code pre_lecture

--- 07_cli/lib/cli.py
+++ 07_cli/lib/cli.py
@@ -15,68 +15,35 @@ from rich import print
 # *************
 # Interface operations
 def display_welcome():
-    print("[magenta]Hello! Welcome to [/magenta][bold cyan]Pet Minder![/bold cyan]")
+    pass
 
 
 def display_main_menu():
-    print("Main Menu")
-    print("1. Show all pets")
-    print("2. Add a pet")
-    print("3. Exit")
+    pass
 
 
 def get_main_choice():
-    return input("What is your command?")
+    pass
 
 
 def display_all_pets():
-    pets = Pet.get_all_pets()
-    for pet in pets:
-        print(f"{pet.id} | {pet.name} | {pet.species}")
-    print("What would you like to see?")
-    print("1. See more about a pet")
-    print("2. Return to main menu")
-    choice = input()
-    if choice == "1":
-        choose_pet_by_id()
-    else:
-        display_main_menu()
+    pass
 
 
 def choose_pet_by_id():
-    search_id = input("Enter the id of the pet you want to see")
-    pet = Pet.find_pet_by_id(search_id)
-    print(
-        f"Id: {pet.id}, Name: {pet.name}, Species: {pet.species}, Breed: {pet.breed}, Temperament: {pet.temperament}"
-    )
-    display_pet_submenu(pet)
+    pass
 
 
 def display_pet_submenu(pet):
-    print("1. See a pet's jobs")
-    print("2. Add a job to a pet")
-    print("3. Exit")
-    print("What would you like to do?")
-    choice = input()
-    handle_pet_choice(choice, pet)
+    pass
 
 
 def handle_pet_choice(choice, pet):
-
-    if choice == "1":
-        show_pet_jobs(pet)
-    elif choice == "2":
-        add_job_to_pet(pet)
-    else:
-        display_all_pets()
+    pass
 
 
 def show_pet_jobs(pet):
-    jobs = Job.get_jobs_by_pet_id(pet.id)
-    for job in jobs:
-        print(
-            f"{job.id} | {job.request} | {job.date} | {job.notes} | ${job.fee:.2f} || with {Handler.find_handler_by_id(job.handler_id).name}"
-        )
+    pass
 
 
 def add_pet(pet):
@@ -84,21 +51,10 @@ def add_pet(pet):
 
 
 def goodbye():
-    print("Thanks for using Pet Minder!")
+    pass
 
 
 # *************
 # Command line interface
 if __name__ == "__main__":
-    display_welcome()
-    while True:
-        display_main_menu()
-        choice = get_main_choice()
-        print(choice)
-        if choice == "1":
-            display_all_pets()
-        elif choice == "2":
-            add_pet()
-        else:
-            break
-    goodbye()
+    pass
--- 07_cli/lib/helpers.py
+++ 07_cli/lib/helpers.py
@@ -6,23 +6,4 @@ from pick import pick
 
 
 def add_job_to_pet(pet):
-    import ipdb
-
-    handler_names = Handler.get_all_handler_names()
-    title = "Which handler will be taking care of this job?"
-    handler_name, index = pick(handler_names, title)
-    handler = Handler.find_handler_by_name(handler_name)
-    req_type = "What type of job are you requesting?"
-    request_choice, index = pick(["Walk", "Drop-in", "Boarding"], req_type)
-    date_string = input("Enter date and time in the format YYYY-MM-DD HH:MM:SS: ")
-    date = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
-    notes = input("Enter any special notes for the handler")
-    job = Job(
-        request=request_choice,
-        handler_id=handler.id,
-        pet_id=pet.id,
-        date=date,
-        notes=notes,
-        fee=handler.hourly_rate,
-    )
-    job.save()
+    pass
