932bf6ab9d66ec09397f18ffc2c09d6a2fe1caa8
betalantz <betalantz@gmail.com>
2024-09-03 21:59:55 -0700

    adds all CRUD ORM methods to pets 06

--- 06_orm/lib/debug.py
+++ 06_orm/lib/debug.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
-from owner import Owner, CONN, CURSOR
-from pet import Pet, CONN, CURSOR
+from owner import Owner
+from pet import Pet
 
-sql = """
-    DROP TABLE IF EXISTS pets
-    DROP TABLE IF EXISTS owner
-"""
+# sql = """
+#     DROP TABLE IF EXISTS pets;
+# """
 
-CURSOR.execute(sql)
+# CURSOR.execute(sql)
 
 # Owner.create_table()
 # frank = Owner("frank", "555-555-5555", "frank@gmail.com", "555 Somewhere St.")
 # frank.save()
-
+Pet.drop_table()
 Pet.create_table()
-spot = Pet("spot", "dog", "chihuahua", "feisty", 1)
-spot.save()
 
-import ipdb; ipdb.set_trace()
+try:
+    spot = Pet("spot", "dog", "chihuahua", "feisty", 1)
+    # spot = Pet("spot", "dog", "chihuahua")
+    spot.save()
+except Exception as e:
+    print(f"An error occured while saving: {e}")
+# finally:
+#     CONN.close()
+
+brigid = Pet.create("Brigid", "cat", "tabby", "fiesty", 2)
+
+
+import ipdb
+
+ipdb.set_trace()
